35a1ee1ed6cb9f5f2b1549e91e430dec7fb0bb6e
Pablo Reszczynski <pablore@me.com>
2015-06-14 12:37:41 -0300

    gradiente

--- UChileBulletin.js
+++ UChileBulletin.js
@@ -23,25 +23,57 @@ if (Meteor.isClient){
 
       var title = event.target.title.value;
       var desc = event.target.desc.value;
-      var date = event.target.date.value;
-      var time = event.target.time.value;
+      var date = event.target.date.value.split('-'); //(year-month-day)
+      var time = event.target.time.value.split(':');
+
+      var meses = {
+        '01': "Enero",
+        '02': "Febrero",
+        '03': "Marzo",
+        '04': "Abril",
+        '05': "Mayo",
+        '06': "Junio",
+        '07': "Julio",
+        '08': "Agosto",
+        '09': "Septiembre",
+        '10': "Octubre",
+        '11': "Noviembre",
+        '12': "Diciembre" 
+      };
+
+      var key = date[1];
+      var str = meses[''+key];
+      console.log(str);
+      var newDate = date[2] + " de " + str;
   
       Tasks.insert({
         title: title,
         descripcion: desc,
-        date: date,
+        date: newDate, 
         time: time,
         createAt: new Date() //current time
       });
+
+      date = new Date(date[0], date[1] - 1, date[2]);
+      if (time) {
+        date = new Date(date.setHours(time[0], time[1]));
+      }
+
+      Calendar.insert({
+        title: title + '',
+        start: date,
+        allDay: false,
+        description: desc + ''
+      });
       
-      //Clear form
-      event.target.title.value = "";
-      event.target.desc.value = "";
-      event.target.date.value = "";
-      event.target.time.value = "";
+      // //Clear form
+      // event.target.title.value = "";
+      // event.target.desc.value = "";
+      // event.target.date.value = "";
+      // event.target.time.value = "";
 
       //prevent default form submit
-      return false;
+      return true;
     },
         
       "click .news": function(){
